92c02b594c447014093f1128e611374ea2556cb9
blyking <46658092+blyking@users.noreply.github.com>
2020-03-21 15:09:19 -0400

    Update Intro.js

--- Intro.js
+++ Intro.js
@@ -1,15 +1,16 @@
-const heading = document.querySelector("#heading");
-const mode = document.querySelector("#mode_selection");
+const mode = document.querySelector("#mode_selection"); //from first textbox
+
+const heading = document.querySelector("#heading"); //will be title of second textbox
 
 mode.addEventListener("keyup", () => {
-    const m = Number.parseInt(mode, 10);
+    const m = Number.parseInt(mode.value, 10);
     if (m == 1) {
         //Practice mode: learn where keys are on keyboard
-        heading.textContent = "Practice Mode";
+        heading.textContent = "Practice Mode"; //1 = practice mode, set heading
     } else if (m == 2) {
         //Test mode: practice typing words and sentences
-        heading.textContent = "Test Mode";
+        heading.textContent = "Test Mode"; //2 = test mode, set heading
     } else {
-        heading.textConstant = "Invalid mode! Please type 1 or 2.";
+        heading.textContent = "Invalid mode! Please type 1 or 2."; //can only type 1 or 2, set heading
     }
 });
